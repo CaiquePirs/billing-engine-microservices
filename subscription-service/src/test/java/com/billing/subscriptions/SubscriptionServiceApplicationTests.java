7203b2327ea96bb5cc9f976787352fe05edcbace
Caique Pires <c.pires@ba.estudante.senai.br>
2026-05-17 20:38:00 +0100

    feat(subscription-service): Implements subscription service and database

---
+++ subscription-service/src/test/java/com/billing/subscriptions/SubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
